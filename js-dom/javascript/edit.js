81826dc2f2a64c1dea5941f85991cef69a0bdaa5
Bikop1 <sir.nik-ita@yandex.ru>
2025-11-12 02:25:11 +0300

    Update edit.js

--- js-dom/javascript/edit.js
+++ js-dom/javascript/edit.js
@@ -1,1 +1,11 @@
+function find_edit() {
+    const secondName_node = document.getElementById('second_name')
+    secondName_node.innerHTML = "<b>Omelchenko</b>"
+    const firstName_node = document.getElementById('first_name')
+    firstName_node.innerHTML = "<b>Nikita</b>"
+    const thirdName_node = document.getElementById('third_name')
+    thirdName_node.innerHTML = "<b>Vadimovich</b>"
+}
 
+const node_for_click = document.getElementById("knopka")
+node_for_click.addEventListener("click", find_edit)
