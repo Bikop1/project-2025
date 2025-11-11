0b72195707edb74d531da1fcacbc7b42f7ab7de7
Bikop1 <sir.nik-ita@yandex.ru>
2025-11-12 02:25:55 +0300

    Update edit.js

--- js-dom/passport_read/edit.js
+++ js-dom/passport_read/edit.js
@@ -1,1 +1,11 @@
+function find_edit() {
+    const secondName_node = document.getElementById('secondName')
+    secondName_node.innerHTML = "<b>Omeclchenko</b>"
+    const firstName_node = document.getElementById('firstName')
+    firstName_node.innerHTML = "<b>Nikita</b>"
+    const year_node = document.getElementById('year')
+    year_node.innerHTML = "<b>2006</b>"
+}
 
+const node_for_click = document.getElementById("for_click")
+node_for_click.addEventListener("click", find_edit)
